fd3740977a8511fec07a271297b2a05eda7fd2ee
rampeand <andre.raden+github@gmail.com>
2017-02-09 09:06:33 -0500

    Add wp container and link to db

--- wp-test.sh
+++ wp-test.sh
@@ -2,3 +2,5 @@
 
 #create db container
 sudo docker run --name wordpressdb -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=wordpress -d mysql:5.7
+#create wp container and link to db
+sudo docker run -e WORDPRESS_DB_PASSWORD=password -d --name wordpress --link wordpressdb:mysql  -p 80:80 wordpress
